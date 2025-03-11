6c89ac4ed522ff3e1a4483628c32b12ced38395f
Malithi Jayathilake <202645227+MalithiJayathilake@users.noreply.github.com>
2025-03-11 19:23:35 +0530

    Add files via upload

---
+++ 2025.03.10/script.js
@@ -0,0 +1,63 @@
+console.log("Welcome");
+
+
+let number1 = 10;
+let number2 = 5;
+
+console.log(number1+number2);
+
+var x = 17;
+var y = 9;
+
+console.log(x-y);
+
+//Print numbers 1 to 10
+
+console.log("Print Numbers 1 to 10");
+
+for(let i=1; i<=10; i++)
+{
+	console.log(i);
+}
+
+//Print only the Odd numbers
+
+console.log("Print the Odd Numbers");
+
+for(let j=1; j<=10; j++)
+{
+	if(j%2==1)
+	{
+		console.log(j);
+	}
+}
+
+//Print the Numbers in reverse
+
+console.log("Print the Numbers in reverse");
+
+for(let k=10; k>=1; k--)
+{
+	console.log(k);
+}
+
+
+//Reverse the numbers in left and right 4321-5-9876
+
+console.log("Reverse the numbers in left and right");
+
+var int j = 9;
+var mid = parseInt(j/2)+1;
+
+for (var i = mid - 1; i > 0; i--)
+{
+	console.log(i)
+}
+
+console.log(mid);
+
+for(var i = j; j > mid; i--)
+{
+	console.log()
+}
+
